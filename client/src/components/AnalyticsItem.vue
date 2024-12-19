243a89230a5ab48cf71ce932b510f34d1eaa3798
Victor Machado <victormachadogp@gmail.com>
2024-12-19 19:15:53 -0300

    feat: add analytics component structure

---
+++ client/src/components/AnalyticsItem.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="flex items-center justify-between relative">
+        <div class="flex items-center gap-2">
+            <div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+            <span class="text-xs whitespace-nowrap	">{{ item.category }}</span>
+        </div>
+        <div class="w-[150px] h-1.5 bg-[#F3F1F1] rounded-full absolute left-[35%]">
+            <div
+            :style="{ backgroundColor: item.color }"
+            class="h-full w-1/2 rounded-full "></div>
+        </div>
+        <div>
+            <span class="text-xs flex">-{{ formatAmount(item.amount) }}</span>
+        </div>
+    </div>
+  </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  item: AnalyticsItem
+  total: number
+}>()
+
+const formatAmount = (amount: number): string => {
+  return new Intl.NumberFormat('en-GB', {
+    style: 'currency',
+    currency: 'GBP',
+    minimumFractionDigits: 2
+  }).format(amount)
+}
+
+defineOptions({
+  name: 'AnalyticsItem'
+})
+</script>
---
+++ client/src/components/AnalyticsSummary.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="w-[420px] mt-[55px]">
+      <h2 class="font-medium mb-4">Analytics</h2>
+      
+      <div class="bg-white flex flex-col px-7 py-8 gap-2 rounded-[0.3rem]">        
+
+        <AnalyticsItem 
+        v-for="item in analyticsData"
+        :key="item.category"
+        :item="item"
+        />
+        
+        <div class="flex justify-between text-xs font-medium border-t mt-4 pt-3">
+           <span>Total</span>
+           <span>-464.53</span> 
+        </div>
+      </div>
+    </div>
+  </template>
+
+  <script setup lang="ts">
+  import { ref } from 'vue'
+  import AnalyticsItem from './AnalyticsItem.vue'
+
+  const analyticsData = ref([
+  {
+    category: 'Meal Service',
+    amount: 406.29,
+    color: '#22c55e'
+  },
+  {
+    category: 'Shopping',
+    amount: 96.29,
+    color: '#a855f7'
+  },
+  {
+    category: 'General',
+    amount: 204.53,
+    color: '#3b82f6'
+  }
+])
+  
+  </script>
