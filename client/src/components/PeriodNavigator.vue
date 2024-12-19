7d39a90ba2fe2dc7f0f2224e792fb822223468e6
Victor Machado <victormachadogp@gmail.com>
2024-12-19 16:49:25 -0300

    feat: Add PeriodNavigator component

---
+++ client/src/components/PeriodNavigator.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex gap-6 self-start">
+        <button>
+            <ArrowLeft />
+        </button>
+        <h2 class="font-medium">{{ currentMonth }}</h2>
+        <button>
+            <ArrowRight />
+        </button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import ArrowLeft from './icons/ArrowLeft.vue'
+import ArrowRight from './icons/ArrowRight.vue'
+
+const currentMonth = new Date().toLocaleString('default', { month: 'long', year: 'numeric' })
+
+</script>
+
+<style scoped>
+
+</style>
