66f218c14e23b9dcd5ac55233b41197f38a1be4d
Jonathan <jonathanfmach@gmail.com>
2024-12-20 22:38:50 +0000

    feat: add month filtering to transactions and category analytics endpoints

--- server/tests/test_finance_service.py
+++ server/tests/test_finance_service.py
@@ -1,4 +1,5 @@
 from sqlmodel import Session
+from src.dtos import MonthYear
 from src.models import Account
 from src.services.finances import FinanceService
 from datetime import datetime
@@ -35,6 +36,42 @@ def test_create_transaction(db_session: Session):
 
     assert transaction.id is not None
     assert transaction.description == "Test Transaction"
+
+
+def test_get_transactions(db_session: Session, account: Account):
+    service = FinanceService(db_session)
+
+    """GIVEN a set of transactions"""
+    date = datetime.now()
+    acc_id = account.id
+    service.create_transaction(date=date, description="Test 1", amount=Decimal("100.00"), account_id=acc_id)
+    service.create_transaction(date=date, description="Test 2", amount=Decimal("50.00"), account_id=acc_id)
+
+    """WHEN fetching transactions"""
+    transactions = service.get_transactions()
+
+    """THEN the response should contain the transactions"""
+    assert len(transactions) == 2
+
+
+def test_get_transactions_by_month(db_session: Session, account: Account):
+    service = FinanceService(db_session)
+
+    """GIVEN a set of transactions"""
+    acc_id = account.id
+    jan2025 = datetime(2025, 1, 1)
+    service.create_transaction(date=jan2025, description="Test 1", amount=Decimal("100.00"), account_id=acc_id)
+    service.create_transaction(date=jan2025, description="Test 2", amount=Decimal("50.00"), account_id=acc_id)
+    dec2024 = datetime(2024, 12, 1)
+    service.create_transaction(date=dec2024, description="Test 2", amount=Decimal("50.00"), account_id=acc_id)
+
+    """WHEN fetching transactions"""
+    transactions = service.get_transactions(month=MonthYear(year=2025, month=1))
+
+    """THEN the response should contain the transactions"""
+    assert len(transactions) == 2
+    assert transactions[0].date == jan2025
+    assert transactions[1].date == jan2025
 
 
 def test_get_category_analytics(db_session: Session, account: Account):
@@ -76,3 +113,35 @@ def test_get_category_analytics(db_session: Session, account: Account):
 
     assert res.categories[2].id is None
     assert res.categories[2].total == Decimal("50.00")
+
+
+def test_get_category_analytics_by_month(db_session: Session, account: Account):
+    service = FinanceService(db_session)
+
+    """GIVEN a set of transactions"""
+    cat1 = service.create_category(title="Cat1", icon="bookmark")
+    cat2 = service.create_category(title="Cat2", icon="bookmark")
+
+    jan2025 = datetime(2025, 1, 1)
+    acc_id = account.id
+    service.create_transaction(  # Category 1
+        date=jan2025, description="", amount=Decimal("100.00"), category_id=cat1.id, account_id=acc_id
+    )
+    service.create_transaction(  # Category 2
+        date=jan2025, description="", amount=Decimal("100.00"), category_id=cat2.id, account_id=acc_id
+    )
+
+    service.create_transaction(  # No Category
+        date=jan2025, description="", amount=Decimal("50.00"), category_id=None, account_id=acc_id
+    )
+    dec2024 = datetime(2024, 12, 1)  # Should not be included
+    service.create_transaction(  # Category 1
+        date=dec2024, description="", amount=Decimal("500.00"), category_id=cat1.id, account_id=acc_id
+    )
+
+    """WHEN fetching category analytics"""
+    res = service.get_category_analytics(month=MonthYear(year=2025, month=1))
+
+    """THEN the response should contain the total amount"""
+    assert res.total == Decimal("250.00")
+    assert len(res.categories) == 3
