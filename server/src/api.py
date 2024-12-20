361f8aa8aba85b87805bc82a5b9c85b8c6326140
Jonathan <jonathanfmach@gmail.com>
2024-12-20 22:41:35 +0000

    feat: update API endpoints to specify return types for transactions and categories

--- server/src/api.py
+++ server/src/api.py
@@ -1,7 +1,8 @@
-from typing import Optional
+from typing import Optional, Sequence
 from fastapi import APIRouter, Depends, Query
 from src.dtos import MonthYear
-from src.services.finances import FinanceService
+from src.models import Category, Transaction
+from src.services.finances import CategoryAnalyticsRespose, FinanceService
 from src.dependencies import finance_service
 
 router = APIRouter()
@@ -24,14 +25,14 @@ def to_month_year(month: Optional[str]) -> Optional[MonthYear]:
 def get_transactions(
     finance_service: FinanceService = Depends(finance_service),
     month: Optional[str] = YearMonthQuery,
-):
+) -> Sequence[Transaction]:
     month_filter = to_month_year(month)
     transactions = finance_service.get_transactions(month=month_filter)
     return transactions
 
 
 @router.get("/categories")
-def get_categories(finance_service: FinanceService = Depends(finance_service)):
+def get_categories(finance_service: FinanceService = Depends(finance_service)) -> Sequence[Category]:
     return finance_service.get_categories()
 
 
@@ -39,6 +40,6 @@ def get_categories(finance_service: FinanceService = Depends(finance_service)):
 def get_analytics(
     finance_service: FinanceService = Depends(finance_service),
     month: Optional[str] = YearMonthQuery,
-):
+) -> CategoryAnalyticsRespose:
     month_filter = to_month_year(month)
     return finance_service.get_category_analytics(month=month_filter)
