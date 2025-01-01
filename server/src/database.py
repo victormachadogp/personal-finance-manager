d23560cab355f49eb9ff1d73929e2d483bc042b7
Jonathan <jonathanfmach@gmail.com>
2025-01-01 16:17:36 +0000

    chore: add bcp_47_lang_tag to Currency model and update seed data

--- server/src/database.py
+++ server/src/database.py
@@ -19,7 +19,6 @@ def get_session(engine):
         return session
 
 
-
 def seed_db():
     engine = get_engine()
     session = get_session(engine)
@@ -38,10 +37,10 @@ def seed_currencies(session: Session):
     print("Seeding database with currencies")
 
     # Seed currencies
-    usd = Currency(id="usd", name="United States Dollar", code="USD", symbol="$")
-    gbp = Currency(id="gbp", name="British Pound", code="GBP", symbol="£")
-    eur = Currency(id="eur", name="Euro", code="EUR", symbol="€")
-    brl = Currency(id="brl", name="Brazilian Real", code="BRL", symbol="R$")
+    usd = Currency(id="usd", name="United States Dollar", symbol="$", bcp_47_lang_tag="en-US")
+    gbp = Currency(id="gbp", name="British Pound", symbol="£", bcp_47_lang_tag="en-GB")
+    eur = Currency(id="eur", name="Euro", symbol="€", bcp_47_lang_tag="und-EU")
+    brl = Currency(id="brl", name="Brazilian Real", symbol="R$", bcp_47_lang_tag="pt-BR")
     session.add(usd)
     session.add(gbp)
     session.add(eur)
